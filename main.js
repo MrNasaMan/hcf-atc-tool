6fda904ddd19c2bd4e0d90eefcf770c8cb1ad8a4
MrNasaMan <davlasvegas@gmail.com>
2026-09-14 13:04:17 -0700

    Add electron-updater logic to main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
+
+// Configure updater logging & automatic download behavior
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 800,
     height: 600,
-    icon:'logo.png',
+    icon: 'logo.png',
     webPreferences: {
       nodeIntegration: true,
     },
@@ -24,12 +29,38 @@ function createWindow() {
   });
 
   mainWindow.loadFile('index.html');
-  
 
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
+
+  // Check for updates after the window is shown
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
+
+// --- Auto-Updater Event Listeners ---
+autoUpdater.on('update-available', () => {
+  console.log('Update available. Downloading in background...');
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Ready',
+    message: `Version ${info.version} has been downloaded. Restart the application now to apply updates?`,
+    buttons: ['Restart Now', 'Later']
+  }).then((returnValue) => {
+    if (returnValue.response === 0) {
+      autoUpdater.quitAndInstall();
+    }
+  });
+});
+
+autoUpdater.on('error', (err) => {
+  console.error('Auto-updater error:', err);
+});
 
 app.whenReady().then(createWindow);
 
